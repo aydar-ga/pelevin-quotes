9d9670378936e93fc564c41d45ac44e64378e932
aydar-ga <aydarcyber@gmail.com>
2024-10-23 21:16:06 +0200

    added logs to test api

--- app/api/randomQuote/route.ts
+++ app/api/randomQuote/route.ts
@@ -1,8 +1,15 @@
 import { NextResponse } from "next/server";
-import { supabase } from "../../../lib/supabaseClient";
+import { supabase } from "../../../../lib/supabaseClient";
 
 export async function GET() {
   try {
+    console.log("Environment Variables in Production:");
+    console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+    console.log(
+      "Supabase Anon Key:",
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    );
+
     // Fetch all quotes from Supabase
     const { data: quotes, error } = await supabase.from("quotes").select("*");
 
@@ -11,14 +18,15 @@ export async function GET() {
       throw new Error("Error fetching quotes from Supabase");
     }
 
-    console.log("Fetched quotes:", quotes);
+    console.log("Fetched quotes:", quotes); // Log the quotes fetched
 
     // Pick a random quote
     if (quotes && quotes.length > 0) {
       const randomIndex = Math.floor(Math.random() * quotes.length);
       const randomQuote = quotes[randomIndex];
+      console.log("Selected quote:", randomQuote);
 
-      // create response with no-cache headers
+      // Create response with no-cache headers
       const response = NextResponse.json(randomQuote);
       response.headers.set("Cache-Control", "no-store, max-age=0");
       return response;
